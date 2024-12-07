048321fb0f5b6137e27183d5127206c280948aaa
Mikko Puustinen <mikko@hey.com>
2024-12-07 18:55:55 +0100

    Day 03

--- src/aoc24/__init__.py
+++ src/aoc24/__init__.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 
-def read_input_file(filename: str):
-    return (
-        (Path(__file__).parent.parent.parent / f"inputs/{filename}")
-        .read_text()
-        .splitlines()
-    )
+def read_input_file(filename: str) -> list[str]:
+    return get_file_contents(filename).splitlines()
+
+
+def get_file_contents(filename: str) -> str:
+    return (Path(__file__).parent.parent.parent / f"inputs/{filename}").read_text()
