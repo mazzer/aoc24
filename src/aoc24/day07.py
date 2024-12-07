097ee2057fea008424f2b7ceff28d3451169f160
Mikko Puustinen <mikko@hey.com>
2024-12-07 19:55:33 +0100

    Day 07

---
+++ src/aoc24/day07.py
@@ -0,0 +1,60 @@
+import itertools
+
+from aoc24 import read_input_file
+
+OPERATORS = ["+", "*"]
+
+
+def read_file() -> list[tuple[int, list[int]]]:
+    result = []
+
+    for line in read_input_file("day07"):
+        expected_sum, line = line.split(": ")
+
+        result.append((int(expected_sum), list(map(int, line.split()))))
+
+    return result
+
+
+def check_line(expected_sum: int, line: list[int]) -> bool:
+    for operators in itertools.product(OPERATORS, repeat=len(line) - 1):
+        result = line[0]
+
+        for i, op in enumerate(operators, start=1):
+            match op:
+                case "+":
+                    result += line[i]
+                case "-":
+                    result -= line[i]
+                case "*":
+                    result *= line[i]
+                case "/":
+                    if line[i] == 0:
+                        break
+
+                    result /= line[i]
+
+            if result > expected_sum:
+                break
+
+        if result == expected_sum:
+            return True
+
+    return False
+
+
+def part1() -> int:
+    r = 0
+    for expected_sum, line in read_file():
+        if check_line(expected_sum, line):
+            r += expected_sum
+
+    return r
+
+
+def part2(): ...
+
+
+def solve():
+    print(part1())
+    print(part2())
