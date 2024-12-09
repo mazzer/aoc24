d4492c377a9eba6e97b9e7323121c5d8ba93ca6a
Mikko Puustinen <mikko@hey.com>
2024-12-09 23:44:10 +0100

    Day 09 part 2

--- src/aoc24/day09.py
+++ src/aoc24/day09.py
@@ -1,46 +1,125 @@
 from aoc24 import read_input_file
 
+EMPTY_SPACE = -1
 
-def read_file() -> list[str]:
-    return read_input_file("day09")
+
+def read_file() -> str:
+    return read_input_file("day09")[0]
+
+
+def generate_blocks():
+    disk_map = read_file()
+
+    file_idx = 0
+    blocks = []
+    for i, length in enumerate(map(int, disk_map)):
+        if i % 2 == 0:
+            blocks.append((file_idx, length))
+            file_idx += 1
+        else:
+            blocks.append((EMPTY_SPACE, length))
+
+    return blocks
+
+
+def move_blocks(blocks, idx_first_empty_space, idx_file_block):
+    size_difference = blocks[idx_first_empty_space][1] - blocks[idx_file_block][1]
+
+    if size_difference == 0:
+        blocks[idx_file_block], blocks[idx_first_empty_space] = (
+            blocks[idx_first_empty_space],
+            blocks[idx_file_block],
+        )
+
+        idx_file_block -= 1
+    elif size_difference > 0:
+        file_block = blocks.pop(idx_file_block)
+
+        blocks[idx_first_empty_space] = (EMPTY_SPACE, size_difference)
+        blocks.insert(idx_first_empty_space, file_block)
+        blocks.insert(idx_file_block + 1, (EMPTY_SPACE, file_block[1]))
+
+        idx_file_block -= 1
+    elif size_difference < 0:
+        file_block = blocks[idx_file_block]
+        empty_block = blocks.pop(idx_first_empty_space)
+
+        blocks.insert(idx_first_empty_space, (file_block[0], empty_block[1]))
+        blocks[idx_file_block] = (file_block[0], file_block[1] - empty_block[1])
+        blocks.insert(idx_file_block + 1, empty_block)
+
+    return idx_file_block
 
 
 def part1() -> int:
-    s = 0
+    blocks = generate_blocks()
 
-    for disk_map in read_file():
-        file_idx = 0
-        blocks = []
+    first_empty_block = 0
+    i = len(blocks) - 1
 
-        for i, length in enumerate(map(int, disk_map)):
-            if i % 2 == 0:
-                blocks += [(str(file_idx),) for _ in range(length)]
-                file_idx += 1
-            else:
-                blocks += [(".",) for _ in range(length)]
+    while True:
+        if blocks[i][0] == EMPTY_SPACE:
+            i -= 1
+            continue
 
-        last_index = 0
-        for i in reversed(range(len(blocks))):
-            if blocks[i] == ".":
-                continue
+        first_empty_block = next(
+            i
+            for i in range(first_empty_block, len(blocks))
+            if blocks[i][0] == EMPTY_SPACE
+        )
 
-            last_index = next(
-                i for i in range(last_index, len(blocks)) if blocks[i][0] == "."
-            )
+        if first_empty_block is None or first_empty_block > i:
+            i -= 1
 
-            if last_index > i:
-                break
+            break
 
-            blocks[i], blocks[last_index] = blocks[last_index], blocks[i]
+        i = move_blocks(blocks, first_empty_block, i)
 
-        s += sum(int(b) * i for i, (b, *_) in enumerate(blocks) if b != ".")
+    return sum(
+        i * v
+        for i, v in enumerate(
+            val
+            for value, length in blocks
+            for val in [value] * length
+            if value != EMPTY_SPACE
+        )
+    )
 
-    return s
+
+def part2() -> int:
+    blocks = generate_blocks()
+
+    i = len(blocks) - 1
+
+    while i:
+        if blocks[i][0] == EMPTY_SPACE:
+            i -= 1
+            continue
+
+        first_empty_block = next(
+            (
+                idx
+                for idx in range(0, len(blocks))
+                if blocks[idx][0] == EMPTY_SPACE and blocks[idx][1] >= blocks[i][1]
+            ),
+            None,
+        )
+
+        if first_empty_block is None or first_empty_block > i:
+            i -= 1
+            continue
+
+        i = move_blocks(blocks, first_empty_block, i)
+
+    return sum(
+        i * v
+        for i, v in enumerate(
+            val for value, length in blocks for val in [value] * length
+        )
+        if v != EMPTY_SPACE
+    )
 
 
-def part2(): ...
-
-
-if __name__ == "__main__":
+def solve():
     print(part1())
     print(part2())
