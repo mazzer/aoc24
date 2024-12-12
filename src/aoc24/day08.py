d982a3101fe5b6140f97910c564361eff4b7d386
Mikko Puustinen <mikko@hey.com>
2024-12-12 11:44:56 +0100

    Day 08 part 2

--- src/aoc24/day08.py
+++ src/aoc24/day08.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from math import gcd
 
 from aoc24 import read_input_file
 
@@ -7,17 +8,24 @@ def read_file() -> list[str]:
     return read_input_file("day08")
 
 
-def part1():
-    grid = [list(line) for line in read_file()]
-    rows, cols = len(grid), len(grid[0])
-
+def get_antennas(grid):
     antennas = defaultdict(list)
 
-    for row in range(rows):
-        for col in range(cols):
+    for row, line in enumerate(grid):
+        for col, cell in enumerate(grid):
             cell = grid[row][col]
             if cell != ".":
                 antennas[cell].append((row, col))
+
+    return antennas
+
+
+def part1():
+    grid = [list(line) for line in read_file()]
+
+    antennas = get_antennas(grid)
+
+    rows, cols = len(grid), len(grid[0])
 
     antinodes = set()
 
@@ -38,7 +46,37 @@ def part1():
     return len(antinodes)
 
 
-def part2(): ...
+def part2():
+    grid = [list(line) for line in read_file()]
+
+    antennas = get_antennas(grid)
+
+    rows, cols = len(grid), len(grid[0])
+
+    antinodes = set()
+
+    for antenna, positions in antennas.items():
+        antinodes.update(positions)
+
+        for i, start in enumerate(positions):
+            for j in range(i + 1, len(positions)):
+                end = positions[j]
+                difference = (end[0] - start[0], end[1] - start[1])
+
+                step_gcd = gcd(difference[0], difference[1])
+                step = (difference[0] // step_gcd, difference[1] // step_gcd)
+
+                current = start
+                while 0 <= current[0] < rows and 0 <= current[1] < cols:
+                    antinodes.add(current)
+                    current = (current[0] - step[0], current[1] - step[1])
+
+                current = end
+                while 0 <= current[0] < rows and 0 <= current[1] < cols:
+                    antinodes.add(current)
+                    current = (current[0] + step[0], current[1] + step[1])
+
+    return len(antinodes)
 
 
 def solve():
